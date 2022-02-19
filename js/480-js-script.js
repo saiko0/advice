27c384a6da7a38d94297311a07dece080991b6bf
saiko0 <kabo2g@gmail.com>
2022-02-19 22:37:57 +0200

    first commit

---
+++ js/480-js-script.js
@@ -0,0 +1,246 @@
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      autoplay:true,
+      
+    autoplayHoverPause:true,
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
+
+
+
+
+(function ($) {
+  'use strict';
+
+
+  // test
+  
+
+
+  // Preloader js    
+  $(window).on('load', function () {
+    $('.preloader').fadeOut(700);
+  });
+
+  //   navfixed
+  $(window).on('scroll', function () {
+    var scrolling = $(this).scrollTop();
+
+    if (scrolling > 10) {
+      $('.naviagtion').addClass('nav-bg');
+    } else {
+      $('.naviagtion').removeClass('nav-bg');
+    }
+  });
+
+  // Background-images
+  $('[data-background]').each(function () {
+    $(this).css({
+      'background-image': 'url(' + $(this).data('background') + ')'
+    });
+  });
+
+  // venobox popup 
+  $('.venobox').venobox();
+
+  //  Count Up
+  function counter() {
+    var oTop;
+    if ($('.counter').length !== 0) {
+      oTop = $('.counter').offset().top - window.innerHeight;
+    }
+    if ($(window).scrollTop() > oTop) {
+      $('.counter').each(function () {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        }, {
+          duration: 2000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          }
+        });
+      });
+    }
+  }
+  $(window).on('scroll', function () {
+    counter();
+  });
+
+
+  // testimonial
+  $('.testimonial-slider-single').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: false,
+    arrows: true,
+    nextArrow: '<buttton class="nextarrow"></buttton>',
+    prevArrow: '<buttton class="prevarrow"></buttton>'
+  });
+
+
+  // blog slider
+  $('.blog-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: false,
+    arrows: true,
+    nextArrow: '<buttton class="nextarrow"></buttton>',
+    prevArrow: '<buttton class="prevarrow"></buttton>',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $('.category-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: false,
+    arrows: true,
+    nextArrow: '<buttton class="nextarrow"></buttton>',
+    prevArrow: '<buttton class="prevarrow"></buttton>',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.testimonial-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dot: false,
+    autoplay: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+
+
+
+
+  
+  // Aos js
+  AOS.init({
+   
+    once: false, 
+    mirror: true,
+    offset: 250,
+    easing: 'ease',
+    duration: 800,
+    anchorPlacement: 'top-bottom',
+  });
+
+  
+$(document).ready(function(){
+	// Add minus icon for collapse element which is open by default
+	$(".collapse.show").each(function(){
+		$(this).siblings(".card-header").find(".btn i").html("remove");
+		$(this).prev(".card-header").addClass("highlight");
+	});
+	
+	// Toggle plus minus icon on show hide of collapse element
+	$(".collapse").on('show.bs.collapse', function(){
+		$(this).parent().find(".card-header .btn i").html("remove");
+	}).on('hide.bs.collapse', function(){
+		$(this).parent().find(".card-header .btn i").html("add");
+	});
+	
+	// Highlight open collapsed element 
+	$(".card-header .btn").click(function(){
+		$(".card-header").not($(this).parents()).removeClass("highlight");
+		$(this).parents(".card-header").toggleClass("highlight");
+	});
+});
+
+
+})(jQuery);
